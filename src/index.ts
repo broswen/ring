f4b7abcc78663773253066a0d1193e3627ce8c1e
Brad <broswen@gmail.com>
2022-11-24 20:39:00 -0600

    implement index routing

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import Toucan from "toucan-js"
+import {nodeURL, rendezvousHash} from "./sharding/sharding";
+import {jsonResponse} from "./node/node";
 export {Node} from './node/node'
 
 export interface Config {
@@ -59,8 +61,19 @@ export async function handler(
 		tracesSampleRate: 1.0,
 		environment: env.environment
 	})
-
 	const config = await getConfig(env)
 	const url = new URL(request.url)
-	return new Response('ok')
+	const key = url.pathname.slice(1)
+	const nodeId = `${await rendezvousHash(key, config.clusterSize)}`
+	const nodeUrl = nodeURL(nodeId, key)
+	if (request.method === 'GET') {
+		const id = env.RING.idFromName(nodeId)
+		const obj = env.RING.get(id)
+		return obj.fetch(new Request(nodeUrl, {body: request.body, cf: {cacheTtl: 5}}))
+	} else if (request.method === 'PUT') {
+		const id = env.RING.idFromName(nodeId)
+		const obj = env.RING.get(id)
+		return obj.fetch(new Request(nodeUrl, {body: request.body}))
+	}
+	return jsonResponse({error: 'not allowed'}, 405, '-1')
 }
